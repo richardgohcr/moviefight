c281d9f68a98a6ff4be463f30869b17a30112376
richardgohcr <richard.gohcr@gmail.com>
2020-05-16 03:13:41 +0800

    Added data fetching with debounce functionality

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: '9db0b2b5',
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+const debounce = (cb, delay=1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      cb.apply(null, args);
+    }, delay);
+  };
+};
+//Alogrithm to debounce an input
+const onInput = (event) => {
+  fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 1500));
