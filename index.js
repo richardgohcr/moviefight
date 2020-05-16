30f1c2190571c04113bb799bb2d1cff8f325dc32
richardgohcr <richard.gohcr@gmail.com>
2020-05-16 15:15:12 +0800

    Render expanded summary

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const fetchData = async (searchTerm) => {
 
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
-  <label><b>Search for a Movie</b></label>
-  <input class="input"/>
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
   <div class="dropdown">
-      <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-      </div>
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
   </div>
 `;
 
@@ -45,6 +45,7 @@ const onInput = async (event) => {
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title; //Note that in this function there is a closure scope over movie
+      onMovieSelect(movie);
     });
 
     resultsWrapper.appendChild(option);
@@ -58,3 +59,33 @@ document.addEventListener('click', (event) => {
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: '9db0b2b5',
+      i: movie.imdbID,
+    },
+  });
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media"> 
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        <div>
+      </div>
+    </article>
+  `;
+};
